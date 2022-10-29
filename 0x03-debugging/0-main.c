90c65189bda717e77a519b8f403f03bdd2c6754c
yannicekal7 <yannicekal@gmail.com>
2022-10-29 09:01:01 -0700

    add 0 main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Test function for postive or negative
+ * Owned by Yanicce
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
