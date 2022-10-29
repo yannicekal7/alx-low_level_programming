5534274bbd803f72b89056700acf8e3eaacc17f0
yannicekal7 <yannicekal@gmail.com>
2022-10-29 05:03:47 -0700

    add 0-reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: int to check
+ * owned By Yannice
+ * Return: 0 is sccess
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
