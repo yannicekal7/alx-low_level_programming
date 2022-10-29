789f05f95576db04007cf6a80fa70f6e68fafb82
yannicekal7 <yannicekal@gmail.com>
2022-10-29 08:20:01 -0700

    fix error

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * rint_square - prints a square of size size
- *  @size: size of the square
- *  Return: void
+ * print_square - prints a square of size size
+ * @size: size of the square
+ * Return: void
  */
 
 void print_square(int size)
