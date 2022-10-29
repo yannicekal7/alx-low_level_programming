c0e6304f4d319758a50c96aa2676a038248a1107
yannicekal7 <yannicekal@gmail.com>
2022-10-29 06:18:59 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ * Return: string
+ * */
+void _puts(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
