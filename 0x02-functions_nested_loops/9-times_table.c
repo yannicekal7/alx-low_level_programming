97f8ea7cd792f4da15bd7ae01c1a88eb0ed1a47a
yannicekal7 <yannicekal@gmail.com>
2022-10-30 04:24:15 -0700

    add element

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,28 @@
 #include "main.h"
 
 /**
- * add - Adds two integers
- * @num1: The first integer to be added.
- * @num2: The second integer to be added.
- * Return: The result of the addition.
+ * times_table - Prints the 9 times table, starting with 0.
  */
-int add(int num1, int num2)
+void times_table(void)
 {
-	return (num1 + num2);
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
